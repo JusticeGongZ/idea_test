9381c2cb3cd2f5cbb87b0d33ed381a1039f02a2a
JusticeGongz <417089489@qq.com>
2019-04-12 10:33:01 +0800

    本地仓库提交测试

---
+++ src/main/java/com/Justice/AddUser.java
@@ -0,0 +1,25 @@
+package com.Justice;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author Justice
+ * @Date 2019/4/11
+ * @Description TODO
+ * @Version: 1.0
+ */
+public class AddUser extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String username = request.getParameter("username");
+        System.out.println(username);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
